4bc0422c0302081809bff17af7d60d43440a852a
unnati.maheshvari@automationanywhere.com <unnati.maheshvari@automationanywhere.com>
2024-10-01 16:11:31 +0530

    Console readin

--- hello/main.go
+++ hello/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Text: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered: ", input)
 }
